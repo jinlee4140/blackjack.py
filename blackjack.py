9abc801268693adc4263cb3088af5fc87776c2c6
jinlee4140 <jinlee4140@gmail.com>
2015-09-12 14:50:01 -0400

    correct syntax error

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,17 @@
-class card:
-	def init(self, value, suit):
+class Card:
+	def __init__(self, value, suit):
 		self.value = value
 		self.suit = suit
 		if value in ['2', '3', '4', '5', '6', '7', '8', '9', '10']:
 			self.value = int(value)
 		if value in ['J', 'Q', 'K']:
 			self.value = 10
-		if value == 'A'
+		if value == 'A':
 			self.value = 1
 
-	def value(self):
+	def val(self):
 		return self.value
 
-	
+		
+james = Card('J', 'heart')
+print james.val()
